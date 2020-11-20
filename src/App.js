c4c2f58fe68257c0d9025d879acfae0b34f1be25
anandhakrishnanm <anandhakrishnanmohank@gmail.com>
2020-11-20 10:33:21 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React,{useState,useEffect} from "react"
+import './App.scss';
+
+
+
+const apiUrl = 'https://api.quotable.io/random';
+function App() {
+
+  const [response, setQuote] = useState({content:"Loding...",author:"..."})
+
+
+  useEffect(() => {handleNextButton();},
+  []);
+
+
+  const getRandomQuote = () => {
+    return fetch(apiUrl).then(response => response.json());
+  }
+  
+  async function handleNextButton() {
+    const quoteData = await getRandomQuote();
+    setQuote({content: quoteData.content,
+              author: quoteData.author})
+    
+  }
+    
+  return (
+    <div className="app">
+      <div className="quote-box-outter">
+        <div className="quote-box-inner">
+          <p className="quote-content">{response.content}</p>
+          <p className="quote-author">-{response.author}</p>
+          <div className="quote-button-wrapper">
+            <button className="next-button btn" onClick={handleNextButton}>Next</button>
+            <a href={`https://twitter.com/intent/tweet?text=${response.content}`}
+             target="_blank">
+               <button className="tweet-button btn">Tweet</button>
+            </a>
+         </div>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+
+export default App;
